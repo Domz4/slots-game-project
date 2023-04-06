f65663933362b0ac6813ce568bb3a7869a6c911c
Dominik <domrza99@gmail.com>
2023-04-06 23:47:54 +0200

    feat: backend skeleton

--- src/UI/Button/index.tsx
+++ src/UI/Button/index.tsx
@@ -1,5 +1,6 @@
-import React, { HTMLAttributes, ReactNode } from "react";
-import "./styles.css";
+import React, { FC, HTMLAttributes, ReactNode } from "react";
+import styles from "./styles.module.css";
+
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
     variant: "secondary" | "secondary-alt" | "primary" | "empty";
     children: ReactNode;
@@ -7,24 +8,34 @@ export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
     backgroundColor?: string;
 }
 
-export const Button = ({
+export const Button: FC<ButtonProps> = ({
     size = "md",
     variant,
     children,
     ...props
 }: ButtonProps) => {
     return variant === "primary" ? (
-        <button type="button" className={`btn btn__${variant}`} {...props}>
-            <span className={`btn__shadow__${variant}`}></span>
-            <span className={`btn__edge__${variant}`}></span>
-            <span className={`btn__front__${variant} btn__${size}`}>
+        <button
+            type="button"
+            className={`${styles.btn} ${styles[`btn__${variant}`]}`}
+            {...props}
+        >
+            <span className={`${styles[`btn__shadow__${variant}`]}`}></span>
+            <span className={`${styles[`btn__edge__${variant}`]}`}></span>
+            <span
+                className={`${styles[`btn__front__${variant}`]} ${
+                    styles[`btn__${size}`]
+                }`}
+            >
                 {children}
             </span>
         </button>
     ) : (
         <button
             type="button"
-            className={`btn btn__${variant} btn__${size}`}
+            className={`${styles.btn} ${styles[`btn__${variant}`]} ${
+                styles[`btn__${size}`]
+            }`}
             {...props}
         >
             {children}
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -4,52 +4,45 @@ import Grid from "../../UI/GameList";
 import { Header } from "../../UI/Header";
 import LoadingOverlay from "../../UI/Loading";
 import { Sidebar } from "../../UI/Sidebar";
+import styles from "./styles.module.css";
 
 export const HomePage = () => {
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleSettings = () => {
-        console.log("Settings clicked");
-    };
+  const handleSettings = () => {
+    console.log("Settings clicked");
+  };
 
-    const handleGame = () => {
-        console.log("Game clicked");
-    };
+  const handleGame = () => {
+    console.log("Game clicked");
+  };
 
-    const handleStats = () => {
-        console.log("Stats clicked");
-    };
+  const handleStats = () => {
+    console.log("Stats clicked");
+  };
 
-    const handleLogin = () => {
-        console.log("Login clicked");
-    };
+  const handleLogin = () => {
+    console.log("Login clicked");
+  };
 
-    const handleLogout = () => {
-        console.log("Logout clicked");
-    };
-
-    const handleRegister = () => {
-        console.log("Register clicked");
-    };
-
-    return (
-        <div className="App">
-            <Header
-                onLogin={handleLogin}
-                onLogout={handleLogout}
-                onRegister={handleRegister}
-            />
-            <Sidebar
-                onSettings={handleSettings}
-                onGame={handleGame}
-                onStats={handleStats}
-            />
-            <LoadingOverlay isLoading={isLoading} />
-            <main>
-                <Grid width={100} height={100}>
-                  <div className="first"></div>
-                </Grid>
-            </main>
-        </div>
-    );
+  const handleLogout = () => {
+    console.log("Logout clicked");
+  };
+  const handleRegister = () => {
+    console.log("Register clicked");
+  };
+  return (
+    <div className="App">
+      <main className={styles.container}>
+        <Header onLogin={handleLogin} onLogout={handleLogout} onRegister={handleRegister} />
+        <Sidebar onSettings={handleSettings} onGame={handleGame} onStats={handleStats} />
+        <LoadingOverlay isLoading={isLoading} />
+        <Grid width={100} height={100}>
+          <div className="first">first</div>
+          <div className="first">first</div>
+          <div className="first">first</div>
+        </Grid>
+      </main>
+    </div>
+  );
 };
