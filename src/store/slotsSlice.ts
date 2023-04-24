62044e35922f78a23511ac9552d50cf2f7c358fb
Dominik <domrza99@gmail.com>
2023-04-24 23:27:36 +0200

    feat: redux slots state

---
+++ src/store/slotsSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Texture } from "pixi.js";
+import { ReelData } from "../components/game/types/types";
+
+interface SlotsState {
+  isAssetsLoading: boolean;
+  textures: Texture[];
+  reels: ReelData[];
+}
+
+const initialState: SlotsState = {
+  isAssetsLoading: false,
+  textures: [],
+  reels: [],
+};
+
+export const slotsSlice = createSlice({
+  name: "slots",
+  initialState,
+  reducers: {
+    setIsAssetsLoading: (state, action: PayloadAction<boolean>) => {
+      state.isAssetsLoading = action.payload;
+    },
+    setTextures: (state, action: PayloadAction<Texture[]>) => {
+      state.textures = action.payload;
+    },
+    setReels: (state, action: PayloadAction<ReelData[]>) => {
+      state.reels = action.payload;
+    },
+  },
+});
+
+export const { setIsAssetsLoading, setTextures, setReels } = slotsSlice.actions;
+
+export default slotsSlice.reducer;
